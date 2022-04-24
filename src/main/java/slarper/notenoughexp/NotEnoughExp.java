f8cadbc7d1f77386fe2657d629d857f8efc7618b
SpaceTeleport <naivesimple@outlook.com>
2022-04-25 00:53:15 +0800

    init

---
+++ src/main/java/slarper/notenoughexp/NotEnoughExp.java
@@ -0,0 +1,15 @@
+package slarper.notenoughexp;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NotEnoughExp implements ModInitializer {
+    public static final String MOD_ID = "notenoughexp";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
